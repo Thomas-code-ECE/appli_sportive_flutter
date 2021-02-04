4f256fd63c2f4ce168bb50a370fd4b1c1e0064a8
Thomas COSTA <61493790+Thomas-code-ECE@users.noreply.github.com>
2021-02-04 12:26:46 +0100

    First commit

---
+++ application_running/android/app/src/main/kotlin/com/example/application_running/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.application_running
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ application_running/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'sign_in/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Application de Running',
+      home: LoginScreen(),
+    );
+  }
+}
